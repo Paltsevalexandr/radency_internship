e26dc63785be4132dda7a0165db3f6de902a9ac4
Aleksandr Paltsev <paltsevaleksandr21@gmail.com>
2021-04-28 15:12:58 +0300

    fix conflicts in home_page_view.dart

--- lib/blocs/navigation/navigation_bloc.dart
+++ lib/blocs/navigation/navigation_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
 
 part 'navigation_event.dart';
 part 'navigation_state.dart';
--- lib/ui/home_page_view.dart
+++ lib/ui/home_page_view.dart
@@ -4,7 +4,8 @@ import 'package:radency_internship_project_2/blocs/authentication/authentication
 import 'package:radency_internship_project_2/blocs/transactions/transactions_slider/transactions_slider_bloc.dart';
 import 'package:radency_internship_project_2/ui/widgets/transactions_view/transactions_view.dart';
 import 'package:radency_internship_project_2/utils/routes.dart';
-
+import 'package:radency_internship_project_2/generated/l10n.dart';
+import 'package:radency_internship_project_2/utils/ui_utils.dart';
 import 'widgets/bottom_nav_bar.dart';
 
 class HomePageView extends StatelessWidget {
@@ -15,9 +16,9 @@ class HomePageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(     
         appBar: AppBar(
-          title: const Text('Home'),
+          title: Text(S.current.home),
           actions: <Widget>[
             IconButton(
               key: const Key('homePage_logout_iconButton'),
@@ -28,9 +29,11 @@ class HomePageView extends StatelessWidget {
         ),
         floatingActionButton: floatingAddButton(context),
         body: BlocProvider<TransactionsSliderBloc>(
-          create: (context) => TransactionsSliderBloc()..add(TransactionsSliderInitialize()),
-          child: TransactionsView(),
-        ),
+        create: (context) =>
+          TransactionsSliderBloc()
+            ..add(TransactionsSliderInitialize()),
+        child: TransactionsView(),
+      ),
         bottomNavigationBar: BottomNavBar()
       );
   }
@@ -38,6 +41,9 @@ class HomePageView extends StatelessWidget {
   Widget floatingAddButton(BuildContext context) {
     return FloatingActionButton(onPressed: () {
       Navigator.of(context).pushNamed(Routes.addTransactionPage);
-    }, child: Icon(Icons.add),);
+    }, child: Icon(
+      Icons.add,
+      size: pixelsToDP(context, 90),
+    ));
   }
 }
